4244d8ee7f4df7f77324e6f1f86733c35f4585c7
Chris Barron <CBarron@ipsoft.com>
2017-11-14 11:50:34 +0100

    More files

---
+++ chris-barron-ipsoft/ChocolateyInstalls-Workstation.ps1
@@ -0,0 +1,31 @@
+Set-ExecutionPolicy Unrestricted
+
+choco install adobereader-update -Y
+choco install putty.install -Y
+choco install git -Y
+choco install 7zip -Y
+choco install notepadplusplus -Y
+choco install jre8 -Y
+choco install silverlight -Y
+choco install sysinternals -Y
+choco install filezilla -Y
+choco install dotnet4.5 -Y
+choco install mremoteng -Y
+choco install flashplayerplugin -Y
+choco install vmwarevsphereclient -Y
+choco install sql-server-management-studio -Y
+choco install sublimetext3 -Y
+choco install unchecky -Y
+choco install firefox -Y
+choco install adblockplus-firefox -Y
+choco install citrix-receiver -Y
+choco install conemu -Y
+choco install gpg4win -Y
+choco install angryip -Y
+choco install sysinternals -Y
+choco install greenshot -y
+choco install bind-toolsonly -Y
+choco install winscp.install -Y
+choco install processhacker -Y
+
+Set-ExecutionPolicy AllSigned
