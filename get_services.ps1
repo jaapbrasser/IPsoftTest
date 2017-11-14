447c7dc185b0d7fa39316110586a5acffd2c8525
Michael Goulart <MGoulart@ipsoft.com>
2017-11-14 16:49:36 +0100

    Added Code

--- get_services.ps1
+++ get_services.ps1
@@ -3,3 +3,8 @@ get-service
 
 #Commit 2 - Get Service for a specific service name starting with "Win"
 get-service -name "Win*"
+
+#Commit 3 - Get all services in stop state 
+
+get-service 
+Get-Service | Where-Object {$_.Status -eq "stopped"}
